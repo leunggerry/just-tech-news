8dbba90ae62edbb8a25841b88512bcd49973c0eb
gigi.upNorth <leung.gerry@gmail.com>
2022-08-17 18:30:04 -0400

    [node, sequelize] adding post sql option for articles

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+//use Model and Datatypes from sequelize package
+const { Model, DataTypes } = require("sequelize");
+//get the mysql connection from the connneciton.js
+const sequelize = require("../config/connection");
+
+//create the POST model
+class Post extends Model {}
+
+// Define the columns in the POST
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "user",
+          key: "id",
+        },
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
