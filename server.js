86d8ab5c4631ee5a8daccb15095af61536043d24
gigi.upNorth <leung.gerry@gmail.com>
2022-08-28 15:54:35 -0400

    Use helper functions to format strings witin in handle bars template
    Added test for helpers

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+//import helpers
+const helpers = require("./utils/helpers");
+
 const sess = {
   secret: "Super secret secret",
   cookie: {},
@@ -21,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
